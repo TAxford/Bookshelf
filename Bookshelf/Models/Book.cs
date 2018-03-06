a59021ff6b9d2a4803ca77db00ef33bfd0faaa5e
TAxford <t.axford2@yahoo.com>
2018-03-06 10:21:34 -0500

    added Category class

--- Bookshelf/Models/Book.cs
+++ Bookshelf/Models/Book.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -14,5 +15,9 @@ namespace Bookshelf.Models
         public DateTime PublishedDate { get; set; }
         public string Description { get; set; }
         public string ISBN { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
     }
 }

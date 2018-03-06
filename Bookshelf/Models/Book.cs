48fd80c59c4acdc01d8e06a5ff9f54833cf10dc1
TAxford <t.axford2@yahoo.com>
2018-03-06 10:04:46 -0500

    Initial commit

---
+++ Bookshelf/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Bookshelf.Models
+{
+    public class Book
+    {
+        [Key]
+        public int BookID { get; set; }
+        public string Titlt { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Description { get; set; }
+        public string ISBN { get; set; }
+    }
+}
